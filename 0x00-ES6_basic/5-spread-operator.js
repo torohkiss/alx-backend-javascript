e13fd4b8594a82d56643dba067ec37374c0d3367
torohkiss <torohkiss@yahoo.com>
2024-10-05 01:42:57 -0700

    task 5a

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
-const concatArrays = (array1, array2, string) => ([...array1, ...array2, ...string])
+const concatArrays = (array1, array2, string) => ([...array1, ...array2, ...string]);
 
 export default concatArrays;
-
