6eae3b7aa1d954817d08ba4b931d6f13ba40c302
torohkiss <torohkiss@gmail.com>
2024-10-12 00:45:52 +0000

    task 1d

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,10 +2,5 @@ export default function getListStudentIds(arr) {
   if (!Array.isArray(arr)) {
     return [];
   }
-  const myMap = new Map();
-  for (const key of arr) {
-    myMap.set(key.id, key.id);
-  }
-  const newIds = [...myMap.keys()];
-  return newIds;
+  return arr.map(x => x.id)
 }
