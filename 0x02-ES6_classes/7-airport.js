aa374e943404ce8412a1fef52bb2c8a1ddbc7c04
torohkiss <torohkiss@yahoo.com>
2024-10-31 13:48:33 +0000

    task 7a

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,19 +1,21 @@
 export default class Airport {
-	constructor (name, code) {
-		this._name = name
-		this._code = code
-	}
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-	first() {
-		return `Airportttt ${this._code} { _name: '${this._name}', _code: '${this._code}' }`
-	}
-	toString() {
-		return `[object ${this._code}]`
-	}
-	[Symbol.toPrimitive](hint) {
-		if (hint === 'string') {
-			return this.first()
-		}
-		return this.toString()
-	}
+  first() {
+    return `Airportttt ${this._code} { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this.first();
+    }
+    return this.toString();
+  }
 }
