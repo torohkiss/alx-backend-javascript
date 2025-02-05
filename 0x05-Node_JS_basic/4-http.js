fb83bc0374c6c83014a9cf6fe18082d212c097fd
torohkiss <torohkiss@yahoo.com>
2025-02-05 10:41:40 +0000

    task 4d

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,8 +3,9 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  //res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello ALX!');
 });
 
