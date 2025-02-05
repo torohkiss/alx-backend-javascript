ac18b9203acc764c20b1cd62850a13deea26f356
torohkiss <torohkiss@yahoo.com>
2025-02-05 10:55:44 +0000

    task 2d

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,35 +1,27 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  try {
-    const fileContent = fs.readFileSync(path, 'utf-8');
-
-    const lines = fileContent.split('\n')
-      .map((line) => line.trim())
-      .filter((line) => line.length > 0);
-
-    const students = lines.slice(1);
-
-    process.stdout.write(`Number of students: ${students.length}\n`);
-
-    const studentsByField = {};
-
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
-      if (!studentsByField[field]) {
-        studentsByField[field] = [];
-      }
-      studentsByField[field].push(firstname);
-    });
-
-    Object.entries(studentsByField).forEach(([field, studentList]) => {
-      process.stdout.write(
-        `Number of students in ${field}: ${studentList.length}. List: ${studentList.join(', ')}\n`,
-      );
-    });
-  } catch (error) {
-    throw new Error('Cannot load the database');
+module.exports = function countStudents (path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
   }
-}
-
-module.exports = countStudents;
+  // read file
+  const data = fs.readFileSync(path, 'utf8');
+  const dbData = data.split('\n')
+    .map((data) => data.split(','))
+    .filter((data) => data.length === 4 && data[0] !== 'firstname')
+    .map((data) => ({
+      firstName: data[0],
+      lastName: data[1],
+      age: data[2],
+      field: data[3]
+    }));
+  const sweField = dbData
+    .filter((data) => data.field === 'SWE')
+    .map((data) => data.firstName);
+  const csField = dbData
+    .filter((data) => data.field === 'CS')
+    .map((data) => data.firstName);
+  console.log(`Number of students: ${sweField.length + csField.length}`);
+  console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
+};
