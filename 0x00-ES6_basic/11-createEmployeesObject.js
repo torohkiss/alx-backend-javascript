232f2d0b74d62666152ac7a4b1d9f7780d896c63
torohkiss <torohkiss@yahoo.com>
2025-04-24 16:29:56 +0100

    task 11

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,13 @@
+export default function createEmployeesObject(departmentName, employees) {
+  /*let i = 0;
+  for (const emp of emmployees){
+    employees[i] = emp;
+    i++;
+  }*/
+
+  const obj = {
+    [`${departmentName}`]: employees,
+  }
+
+  return obj;
+}
