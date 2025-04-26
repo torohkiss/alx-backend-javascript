0c9d8ca389d15e2f0b9630207cc578c3254a6e86
torohkiss <torohkiss@yahoo.com>
2025-04-26 08:44:49 +0100

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-export default function getResponseFromAPI() {
+export default function getResponseFromAPI () {
   return new Promise((resolve) => {
-    resolve('true');
+    resolve();
   });
 }
--- 0x01-ES6_promise/5-photo-reject.js
+++
@@ -1,5 +0,0 @@
-export default function uploadPhoto(fileName) {
-  return new Promise((_, reject) => {
-    reject(new Error(`${fileName} cannot be processed`));
-  });
-}
