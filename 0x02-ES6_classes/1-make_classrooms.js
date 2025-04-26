6378ba40b4937cfc4953a924af12f5581f15ff2a
torohkiss <torohkiss@yahoo.com>
2025-04-26 20:40:55 +0100

    task 0

--- 0x02-ES6_classes/1-make_classrooms.js
+++
@@ -1,9 +0,0 @@
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-  const sizeA = new ClassRoom(19);
-  const sizeB = new ClassRoom(20);
-  const sizeC = new ClassRoom(34);
-
-  return [sizeA, sizeB, sizeC];
-}
