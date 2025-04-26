031cbc176ebc821355d386e99dff0c0e29ab4bde
torohkiss <torohkiss@yahoo.com>
2025-04-26 14:42:31 +0100

    task 3a

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,9 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup () {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+      const photo = results[0];
+      const user = results[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch((error) => {
       console.log('Signup system offline');
