a19b5c8310012ad2753161e238cd61476a7416eb
torohkiss <torohkiss@yahoo.com>
2025-05-10 18:51:12 +0100

    task 1a

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,4 @@
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.charAt(0), ". ").concat(lastName);
+};
+console.log(printTeacher('John', 'Doe'));
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[propName: string]: string | boolean | number | undefined;
+}
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
+	return `${firstName.charAt(0)}. ${lastName}`
+}
+
+console.log(printTeacher('John', 'Doe'))
